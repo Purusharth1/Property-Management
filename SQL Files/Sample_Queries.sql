c8777dc5be2b6dff955a980891fd9ae20b1d0277
AkshayJadhav96 <127526928+AkshayJadhav96@users.noreply.github.com>
2024-07-31 22:16:29 +0530

    Update Sample_Queries.sql

--- SQL Files/Sample_Queries.sql
+++ SQL Files/Sample_Queries.sql
@@ -16,3 +16,26 @@
 -- insurancepolicy.property_id=property.property_id and
 -- insurancepolicy.expiry_date<current_date;
 
+-- Q4)
+-- select name,property.address,market_value,rent_amount
+-- from company,property,lease
+-- where company.company_id=property.company_id and 
+-- property.property_id=lease.property_id and
+-- property.built_year<2000 and
+-- lease.rent_amount>1500
+
+-- Q5)
+-- select tenant.name,company.name,amount
+-- from tenant,lease,payment,company,property
+-- where 
+-- tenant.tenant_id=lease.tenant_id and
+-- lease.lease_id=payment.lease_id and
+-- property.property_id=lease.property_id and
+-- company.company_id=property.company_id and
+-- (payment_method='Bank Transfer' or payment_method='Credit Card'); 
+
+-- select lease_id,lease_start_date from lease ;
+
+-- update payment 
+-- set payment_date='2023-10-17'
+-- where lease_id=10
